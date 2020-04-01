ed2710fc77ec79431f467569a568e8e1dc70ead6
duarte.jr105 <duarte.jr105@gmail.com>
2020-03-31 23:07:24 -0300

    Make it notable

--- Main.kt
+++ Main.kt
@@ -10,11 +10,12 @@ fun main() {
     val status = scanner.nextLine()
     val lenName = lenString(name)
     val lenStatus = status.length
-    val len = if (lenName > lenStatus) lenName else lenStatus + 4
-    val asterisks = CharArray(len) { _ -> '*'}
+    val len = if (lenName > lenStatus) lenName else lenStatus + 6
+    val lenChar = if (len == lenStatus + 6) len else len + 6
+    val asterisks = CharArray(lenChar) { _ -> '*'}
     println(asterisks.joinToString(""))
     printName(name, len)
-    printStatus(status, len)
+    printStatus(status, lenChar)
     println(asterisks.joinToString(""))
 }
 
@@ -38,7 +39,7 @@ fun alphabet(x: Char): Array<String> {
         'O' -> arrayOf("____", "|  |", "|__|")
         'P' -> arrayOf("___ ", "|__]", "|   ")
         'Q' -> arrayOf("____", "|  |", "|_\\|")
-        'R' -> arrayOf("____", "|__/", "| \\ ")
+        'R' -> arrayOf("____", "|__/", "|  \\")
         'S' -> arrayOf("____", "[__ ", "___]")
         'T' -> arrayOf("___", " | ", " | ")
         'U' -> arrayOf("_  _", "|  |", "|__|")
@@ -53,36 +54,38 @@ fun alphabet(x: Char): Array<String> {
 
 fun printName(string: String, len: Int) {
     var n = 0
-    val whiteSpace = (len - lenString(string) + 2) / 2
+    var whiteSpace = (len - lenString(string)) / 2 - 1
+    whiteSpace = if (whiteSpace <= 1) 2 else whiteSpace
     val spaces = CharArray(whiteSpace) { _ -> ' '}
     for (n in 0..2) {
         print("*")
         print(spaces.joinToString(""))
-        for (i in string) {
-            val m = alphabet(i)
+        for (i in string.indices) {
+            val m = alphabet(string[i])
             print(m[n])
-            print(" ")
+            if (i != string.length -1 ) print(" ")
         }
         print(spaces.joinToString(""))
+        if (lenString((string)) + 2 * whiteSpace + 2 < len) print(" ")
         println("*")
     }
 }
 
 fun lenString(string: String): Int {
-    var len = 4
+    var len = 0
     for (i in string) {
         val m = alphabet(i)
         len += m[0].length + 1
     }
-    return len
+    return len - 1
 }
 
 fun printStatus(string: String, len: Int) {
-    print("*")
-    val spaces = CharArray( (len - 2 - string.length) / 2) { _ -> ' '}
+    print("*  ")
+    val spaces = CharArray( (len - string.length - 6) / 2) { _ -> ' '}
     print(spaces.joinToString(""))
     print(string)
-    if (2*spaces.size + 2 + string.length < len) print(" ")
+    if (2*spaces.size + 6 + string.length < len) print(" ")
     print(spaces.joinToString(""))
-    println("*")
+    println("  *")
 }
