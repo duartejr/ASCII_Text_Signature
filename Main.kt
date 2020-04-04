b7da8916337b93eb2b3db385ee3a85ae15a6d728
duarte.jr105 <duarte.jr105@gmail.com>
2020-04-04 14:42:55 -0300

    almost there

--- Main.kt
+++ Main.kt
@@ -1,25 +1,100 @@
 package signature
 
-import java.io.File
 import java.util.*
-import kotlin.reflect.typeOf
 
 fun main() {
     val scanner = Scanner(System.`in`)
     println("Enter name and surname: ")
     val name = scanner.nextLine()
-    printName(name, 10)
+    val lenName = lenString(name, "roman")
+    println("Enter person's status: ")
+    val status = scanner.nextLine().toUpperCase()
+    val lenStatus = lenString(status, "alphabet")
+    val asterisks = CharArray(lenName.coerceAtLeast(lenStatus) + 8) { _ -> '8'}
 
-    /*println("Enter person's status: ")
-    val status = scanner.nextLine()
-    val lenName = lenString(name)
-    val lenStatus = status.length
-    val len = if (lenName > lenStatus) lenName else lenStatus + 4
-    val asterisks = CharArray(len) { _ -> '*'}
     println(asterisks.joinToString(""))
+    printName(name, lenName, lenStatus)
+    printStatus(status, lenName, lenStatus)
+    println(asterisks.joinToString(""))
+}
 
-    printStatus(status, len)
-    println(asterisks.joinToString(""))*/
+fun lenString(string: String, dict: String): Int {
+    var len = 0
+    for (i in string) {
+        val m = if (dict == "roman") roman(i) else alphabet(i)
+        len += m[0].length
+    }
+    return len
+}
+
+fun printName(string: String, lenName: Int, lenStatus: Int) {
+    val nSpaces = (kotlin.math.abs(lenName - lenStatus)) / 2
+    val spaces = CharArray(nSpaces) { _ -> ' '}
+
+    for (n in 0..9) {
+        print("88  ")
+        if (lenStatus > lenName) print(spaces.joinToString(""))
+        for (i in string) {
+            val m = roman(i)
+            print(m[n])
+        }
+        if (lenStatus > lenName) {
+            print(spaces.joinToString(""))
+            if ((lenStatus - lenName) % 2 != 0) print(" ")
+        }
+        println("  88")
+    }
+}
+
+fun printStatus(string: String, lenName: Int, lenStatus: Int) {
+    val nSpaces = (kotlin.math.abs(lenName - lenStatus)) / 2
+    val spaces = CharArray(nSpaces) { _ -> ' '}
+
+    for (i in 0..2) {
+        print("88  ")
+        if (lenName > lenStatus) print(spaces.joinToString(""))
+        for (l in string) {
+            val m = alphabet(l)
+            print(m[i])
+        }
+        if (lenName > lenStatus) {
+            print(spaces.joinToString(""))
+            if ((lenName - lenStatus) % 2 != 0) print(" ")
+        }
+        println("  88")
+    }
+}
+
+fun alphabet(x: Char): Array<String> {
+    return when (x) {
+        'A' -> arrayOf("____ ","|__| ","|  | ")
+        'B' -> arrayOf("___  ", "|__] ", "|__] ")
+        'C' -> arrayOf("____ ", "|    ", "|___ ")
+        'D' -> arrayOf("___  ", "|  \\ ", "|__/ ")
+        'E' -> arrayOf("____ ", "|___ ", "|___ ")
+        'F' -> arrayOf("____ ", "|___ ", "|    ")
+        'G' -> arrayOf("____ ", "| __ ", "|__] ")
+        'H' -> arrayOf("_  _ ", "|__| ", "|  | ")
+        'I' -> arrayOf("_ ", "| ", "| ")
+        'J' -> arrayOf(" _ ", " | ", "_| ")
+        'K' -> arrayOf("_  _ ", "|_/  ", "| \\_ ")
+        'L' -> arrayOf("_    ", "|    ", "|___ ")
+        'M' -> arrayOf("_  _ ", "|\\/| ", "|  | ")
+        'N' -> arrayOf("_  _ ", "|\\ | ", "| \\| ")
+        'O' -> arrayOf("____ ", "|  | ", "|__| ")
+        'P' -> arrayOf("___  ", "|__] ", "|    ")
+        'Q' -> arrayOf("____ ", "|  | ", "|_\\| ")
+        'R' -> arrayOf("____ ", "|__/ ", "|  \\ ")
+        'S' -> arrayOf("____ ", "[__  ", "___] ")
+        'T' -> arrayOf("___ ", " |  ", " |  ")
+        'U' -> arrayOf("_  _ ", "|  | ", "|__| ")
+        'V' -> arrayOf("_  _ ", "|  | ", " \\/  ")
+        'W' -> arrayOf("_ _ _ ", "| | | ", "|_|_| ")
+        'X' -> arrayOf("_  _ ", " \\/  ", "_/\\_ ")
+        'Y' -> arrayOf("_   _ ", " \\_/  ", "  |   ")
+        'Z' -> arrayOf("___  ", "  /  ", " /__ ")
+        else -> arrayOf("     ", "     ", "     ")
+    }
 }
 
 fun roman(x: Char): Array<String> {
@@ -545,65 +620,7 @@ fun roman(x: Char): Array<String> {
                 "              ",
                 "              ")
         else -> arrayOf("          ", "          ", "          ", "          ",
-                        "          ", "          ", "          ", "          ",
-                        "          ", "          ", "          ")
+                "          ", "          ", "          ", "          ",
+                "          ", "          ", "          ")
     }
-}
-fun alphabet(x: Char): Array<String> {
-    return when (x) {
-        'A' -> arrayOf("____","|__|","|  |")
-        'B' -> arrayOf("___ ", "|__]", "|__]")
-        'C' -> arrayOf("____", "|   ", "|___")
-        'D' -> arrayOf("___ ", "|  \\", "|__/")
-        'E' -> arrayOf("____", "|___", "|___")
-        'F' -> arrayOf("____", "|___", "|   ")
-        'G' -> arrayOf("____", "| __", "|__]")
-        'H' -> arrayOf("_  _", "|__|", "|  |")
-        'I' -> arrayOf("_", "|", "|")
-        'J' -> arrayOf(" _", " |", "_|")
-        'K' -> arrayOf("_  _", "|_/ ", "| \\_")
-        'L' -> arrayOf("_   ", "|   ", "|___")
-        'M' -> arrayOf("_  _", "|\\/|", "|  |")
-        'N' -> arrayOf("_  _", "|\\ |", "| \\|")
-        'O' -> arrayOf("____", "|  |", "|__|")
-        'P' -> arrayOf("___ ", "|__]", "|   ")
-        'Q' -> arrayOf("____", "|  |", "|_\\|")
-        'R' -> arrayOf("____", "|__/", "| \\ ")
-        'S' -> arrayOf("____", "[__ ", "___]")
-        'T' -> arrayOf("___", " | ", " | ")
-        'U' -> arrayOf("_  _", "|  |", "|__|")
-        'V' -> arrayOf("_  _", "|  |", " \\/ ")
-        'W' -> arrayOf("_ _ _", "| | |", "|_|_|")
-        'X' -> arrayOf("_  _", " \\/ ", "_/\\_")
-        'Y' -> arrayOf("_   _", " \\_/ ", "  |  ")
-        'Z' -> arrayOf("___ ", "  / ", " /__")
-        else -> arrayOf("    ", "    ", "    ")
-    }
-}
-fun printName(string: String, len: Int) {
-    for (n in 0..9) {
-        print("88 ")
-        for (i in string) {
-            val m = roman(i)
-            print(m[n])
-        }
-        println(" 88")
-    }
-}
-fun lenString(string: String): Int {
-    var len = 4
-    for (i in string) {
-        val m = alphabet(i)
-        len += m[0].length + 1
-    }
-    return len
-}
-fun printStatus(string: String, len: Int) {
-    print("*")
-    val spaces = CharArray( (len - 2 - string.length) / 2) { _ -> ' '}
-    print(spaces.joinToString(""))
-    print(string)
-    if (2*spaces.size + 2 + string.length < len) print(" ")
-    print(spaces.joinToString(""))
-    println("*")
 }
